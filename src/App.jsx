7bb4a34d5193bdf7ea0d5a83e5737098def14448
DocHummus <m.hernandez1115@outlook.com>
2024-01-03 16:15:12 -0500

    Puppy data with uState

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import { useState } from "react";
+import { puppyList } from "./data.js";
 
 function App() {
-  return <div></div>;
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(useState(puppies));
+  return (
+    <div className="App">
+      {puppies.map((puppy) => {
+        return <p key={puppy.id}>{puppy.name}</p>;
+      })}
+    </div>
+  );
 }
 
 export default App;
